13d8a00858b4015701c13ffc2b5932f164a416e0
sandeepdev01 <dev01sandeep@gmail.com>
2026-01-06 01:09:45 +0530

    Add files via upload

---
+++ My-Ecommerce/src/main/java/com/example/My_Ecommerce/MyEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.example.My_Ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyEcommerceApplication.class, args);
+	}
+
+}
---
+++ My-Ecommerce/src/test/java/com/example/My_Ecommerce/MyEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.My_Ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
